059b83bebd2d417453f487f803f4c9f2ab905fe4
AngelaJiaNan <angelanan12@gmail.com>
2021-10-18 11:12:26 -0700

    I have completed dom-querying exercise.

---
+++ dom-querying/main.js
@@ -0,0 +1,22 @@
+console.log('hello, world');
+
+var firstHeading = document.querySelector('h1');
+console.log(firstHeading);
+
+console.dir('h1');
+
+var explanation = document.querySelector('#explanation');
+console.log(explanation);
+
+console.dir('p#explanation');
+
+var hint = document.querySelector('.hint');
+console.log(hint);
+
+console.dir('p.hint');
+
+var allElements = document.querySelectorAll('p');
+console.log(allElements);
+
+var allElements1 = document.querySelectorAll('.example-link');
+console.log(allElements1);
