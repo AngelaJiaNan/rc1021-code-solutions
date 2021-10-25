e7a73ea44764e90a884be0c0e311498567508827
AngelaJiaNan <angelanan12@gmail.com>
2021-10-24 23:16:44 -0700

    I have completed object-manipulation-2 exercise.

---
+++ object-manipulation-2/src/defaults.js
@@ -0,0 +1,10 @@
+/* exported defaults */
+function defaults(target, source) {
+  for (var key in source) {
+    if (key in target) {
+      continue;
+    } else {
+      target[key] = source[key];
+    }
+  }
+}
