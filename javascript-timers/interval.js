adc9dc5ed5c0c9ca704bd5722cb5c89ac2752cba
AngelaJiaNan <angelanan12@gmail.com>
2021-11-03 12:46:36 -0700

    fixed the changes.

--- javascript-timers/interval.js
+++ javascript-timers/interval.js
@@ -1,12 +1,11 @@
 function callBack() {
   var text = document.querySelector('h1');
   var currentNumber = text.textContent;
-
   if (currentNumber > 1) {
     text.textContent = currentNumber - 1;
   } else {
     text.textContent = '~Earth Beeeelooowww Us~';
-    clearInterval();
+    clearInterval(intervalId);
   }
 }
-setInterval(callBack, 1000);
+var intervalId = setInterval(callBack, 1000);
