31af382b5e2d7b5bc5c31f5ce7334d6ea2862ddd
Angela Jia Nan <angelanan12@gmail.com>
2021-12-10 21:35:48 +0000

    I have completed react-function-components exercise.

---
+++ react-function-components/src/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function CustomButton() {
+  return <button>Click me!</button>;
+}
+
+const root = document.querySelector('#root');
+
+ReactDOM.render(CustomButton(), root);
