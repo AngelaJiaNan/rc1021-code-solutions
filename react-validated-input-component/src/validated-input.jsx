f7a47055a6be7228c1ce5f2b189ca077d9622122
Angela Jia Nan <angelanan12@gmail.com>
2021-12-15 19:50:31 +0000

    I have completed react-validated-input-component exercise.

---
+++ react-validated-input-component/src/validated-input.jsx
@@ -0,0 +1,59 @@
+import React from 'react';
+
+class ValidatedInput extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      status: false,
+      password: '',
+      errormsg: 'A password is required'
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    const value = event.target.value;
+    this.setState({ password: value });
+    if (!value) {
+      this.setState({
+        status: false,
+        errormsg: 'A password is required'
+      });
+    } else if (value.length < 8) {
+      this.setState({
+        status: false,
+        errormsg: 'Your password is too short'
+      });
+    } else {
+      this.setState({
+        status: true,
+        errormsg: ''
+      });
+    }
+  }
+
+  render() {
+    // console.log('STATE:', this.state);
+    // console.log('VALUE:', this.state.value);
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <div className="container">
+          <label>
+          Password
+          </label>
+          <div className="passwordbox">
+            <input
+            name="password"
+            type="password"
+            id="pwd"
+            onChange={this.handleChange}/>
+            <i className={this.state.status ? 'fas fa-check' : 'fas fa-times'}></i>
+          </div>
+          <p>{this.state.errormsg}</p>
+        </div>
+      </form>
+    );
+  }
+}
+
+export default ValidatedInput;
